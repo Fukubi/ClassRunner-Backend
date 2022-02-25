a0bebb26c9c9bf519bc21ebd228bc9e072fee921
Fukubi <deividgabrielproc@gmail.com>
2022-02-25 13:22:17 -0300

    fix: Flask CORS fixed

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask_cors import CORS
 from sqlalchemy.ext.declarative import declarative_base
 
 # App used for Flask initialization
 app = Flask(__name__)
-
+CORS(app)
 
 # Base used by sqlalchemy to create tables
 Base = declarative_base()
